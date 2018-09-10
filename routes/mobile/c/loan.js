8a8ad661430fbfca490eeebac97652e7f44e87ae
z80016144 <z80016144@f29e169c-a996-405e-b1be-2f225625e97f>
2018-09-10 09:47:18 +0000

    678902424

--- routes/mobile/c/loan.js
+++ routes/mobile/c/loan.js
@@ -21,8 +21,8 @@ router.use(function (req, res, next) {
     htmlBody.title = "分众链保";
     htmlBody.isLogin = 0;
     if (res.locals.user) {
-        res.locals.currentUser = res.locals.user;
-        res.locals.userId = res.locals.user.id;
+        res.locals.currentUser = res.locals.user.data;
+        res.locals.userId = res.locals.user.data.id;
         htmlBody.isLogin = 1;
     } else {
     	config.toLogin(req, res,0);
